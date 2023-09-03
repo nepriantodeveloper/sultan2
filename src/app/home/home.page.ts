cf38986c97492dc63055a8e400429ed28b5b4566
nepriantodeveloper <neprianto@gmail.com>
2023-09-03 15:04:49 +0700

    kamera ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Camera, CameraResultType } from '@capacitor/camera';
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  gambar:any;
+  @ViewChild('gambarnya') gambarnya!: ElementRef;
   constructor() {}
+
+  foto(){
+    const image = Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl
+    });
+    image.then((res: any) => {
+      this.gambar = res.dataUrl;
+      this.gambarnya.nativeElement.src = this.gambar;
+    })
+  }
 
 }
